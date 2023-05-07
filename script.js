c942aacc3270672cb2d5be44c8a2673c2cf2b445
TomHaene <tom@haene.org>
2023-05-07 17:54:30 -0400

    Added a skills section with JS functionallity. Also added rem font-sizes.

--- script.js
+++ script.js
@@ -1,10 +1,112 @@
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const resumeButton = document.querySelector('.resumebutton')
-    if (resumeButton) {
-      resumeButton.addEventListener('click', function() {
-        alert("Here's my resume")
-      })
-    }
-  })
+
+
+function languagesMenu(){
+
+
+  const languageButton = document.querySelector('[data-language]')
+  const softwareButton = document.querySelector('[data-software]')
+  const hardwareButton = document.querySelector('[data-hardware]')
+  
+  languageButton.style.backgroundColor='#45a049';
+  hardwareButton.style.backgroundColor='lightskyblue';
+  softwareButton.style.backgroundColor='lightskyblue';
+
+  
+
+  document.querySelector('.skill1 h3').textContent = "HTML 5";
+  document.querySelector('.skill1 img').src = "htmlicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill2 h3').textContent = "CSS";
+  document.querySelector('.skill2 img').src = "cssicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill3 h3').textContent = "JavaScript";
+  document.querySelector('.skill3 img').src = "JavaScripticon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill4 h3').textContent = "Java";
+  document.querySelector('.skill4 img').src = "javaicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill5 h3').textContent = "Python";
+  document.querySelector('.skill5 img').src = "pythonicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill6 h3').textContent = "C";
+  document.querySelector('.skill6 img').src = "cicon.png";
+
+  return;
+
+}
+
+
+function softwareMenu(){
+
+
+  const languageButton = document.querySelector('[data-language]')
+  const softwareButton = document.querySelector('[data-software]')
+  const hardwareButton = document.querySelector('[data-hardware]')
+  
+  languageButton.style.backgroundColor='lightskyblue';
+  hardwareButton.style.backgroundColor='lightskyblue';
+  softwareButton.style.backgroundColor='#45a049';
+
+  document.querySelector('.skill1 h3').textContent = "VSCode";
+  document.querySelector('.skill1 img').src = "vscodeicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill2 h3').textContent = "Android Studio";
+  document.querySelector('.skill2 img').src = "androidstudioicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill3 h3').textContent = "Altium Designer";
+  document.querySelector('.skill3 img').src = "altiumicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill4 h3').textContent = "IntelliJ";
+  document.querySelector('.skill4 img').src = "jicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill5 h3').textContent = "Intel Quartus Prime";
+  document.querySelector('.skill5 img').src = "quartusicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill6 h3').textContent = "Spring";
+  document.querySelector('.skill6 img').src = "springicon.svg";
+
+  return;
+
+}
+
+
+
+function hardwareMenu(){
+
+
+  const languageButton = document.querySelector('[data-language]')
+  const softwareButton = document.querySelector('[data-software]')
+  const hardwareButton = document.querySelector('[data-hardware]')
+  
+  languageButton.style.backgroundColor='lightskyblue';
+  hardwareButton.style.backgroundColor='#45a049';
+  softwareButton.style.backgroundColor='lightskyblue';
+
+
+
+
+
+
+  document.querySelector('.skill1 h3').textContent = "Arduino";
+  document.querySelector('.skill1 img').src = "arduinoicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill2 h3').textContent = "Android Studio";
+  document.querySelector('.skill2 img').src = "androidstudioicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill3 h3').textContent = "Altium Designer";
+  document.querySelector('.skill3 img').src = "altiumicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill4 h3').textContent = "IntelliJ";
+  document.querySelector('.skill4 img').src = "jicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill5 h3').textContent = "Intel Quartus Prime";
+  document.querySelector('.skill5 img').src = "quartusicon.png";
+  //------------------------------------------------------------//
+  document.querySelector('.skill6 h3').textContent = "Spring";
+  document.querySelector('.skill6 img').src = "springicon.svg";
+
+  return;
+
+}
